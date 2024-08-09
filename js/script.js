e8575e8973f7f6c565b1793ee3cbd940631f028a
John Doe <ka7de0112@gmail.com>
2024-08-09 22:12:00 +0900

    実装01

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-$(function () {});
+$(function () {
+
+  // ハンバーガーメニュー
+  $('.js-hamburger').click(function(){
+    $(this).toggleClass('is-active');
+    $(".js-drawer").fadeToggle();
+  });
+  $('.drawer__link').click(function(){
+    $('.js-hamburger').removeClass('is-active');
+    $('.js-drawer').fadeToggle();
+  });
+
+});
